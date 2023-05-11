96b5c9da455f37720c29fc912da5921a1feca917
ISNewton <ashrfalhag.11@gmail.com>
2023-05-11 15:44:39 +0200

    finish tasks crud

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts/main')
+
+@section('content')
+    <div class="bg-white p-6">
+
+        <form method="post" action="{{ route('tasks.store') }}" enctype="multipart/form-data">
+            @csrf
+
+            @if ($errors->any())
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li class="text-red-500">{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
+
+            <div class="mb-6">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Name</label>
+                <input type="name" id="name" name="name"
+                    class="p-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                    placeholder="Example" required>
+            </div>
+
+            <button type="submit"
+                class="text-black bg-blue-500  focus:ring-4
+                 focus:outline-none focus:ring-blue-300 font-medium
+                  rounded-lg text-sm w-full sm:w-auto
+                 px-5 py-2.5 text-center ">Submit</button>
+        </form>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -25,6 +26,7 @@ Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', UserController::class);
     Route::resource('clients', ClientController::class);
     Route::resource('projects', ProjectController::class);
+    Route::resource('tasks', TaskController::class);
 });
 
 Route::group(['middleware' => 'guest'], function () {
