53cb9f4ccd57ed14c4fa878c714e80f2a69a9836
ISNewton <ashrfalhag.11@gmail.com>
2023-05-11 15:23:44 +0200

    add client crud

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -5,9 +5,9 @@
             <div
                 class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
                 <div class="p-6 pb-0 mb-0 bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-                    <h6>Users table</h6>
+                    <h6>clients table</h6>
                 </div>
-                <a class="mr-12 rouned-2 bg-red-500 text-black text-right" href="{{ route('users.create') }}">Add</a>
+                <a class="mr-12 rouned-2 bg-red-500 text-black text-right" href="{{ route('clients.create') }}">Add</a>
                 <div class="flex-auto px-0 pt-0 pb-2">
                     <div class="p-0 overflow-x-auto">
                         <table class="items-center w-full mb-0 align-top border-gray-200 text-slate-500">
@@ -16,59 +16,70 @@
                                     <th
                                         class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
                                         ID</th>
-                                        <th
+                                    <th
                                         class="px-6 py-3 pl-2 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
                                         Image</th>
-                                        <th
+                                    <th
                                         class="px-6 py-3 pl-2 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
                                         Name</th>
                                     <th
                                         class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
                                         Email</th>
-
-                                        <th
+                                    <th
+                                        class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
+                                        Phone</th>
+                                    <th
                                         class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
                                         Action</th>
 
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach ($users as $user)
+                                @foreach ($clients as $client)
                                     <tr>
                                         <td
                                             class="p-2 text-center  align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
                                             <span class="text-m font-semibold leading-tight text-slate-400">
-                                                {{ $user->id }}
+                                                {{ $client->id }}
                                             </span>
                                         </td>
                                         <td
                                             class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
                                             <span class="text-m font-semibold leading-tight text-slate-400">
-                                                <img width="80" height="80"  src="{{ $user->imagePath }}" alt="User image">
+                                                <img width="80" height="80" src="{{ $client->imagePath }}"
+                                                    alt="User image">
                                             </span>
                                         </td>
                                         <td
                                             class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
                                             <span class="text-m font-semibold leading-tight text-slate-400">
-                                                {{ $user->name }}
+                                                {{ $client->name }}
                                             </span>
                                         </td>
                                         <td
                                             class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
                                             <span class="text-m font-semibold leading-tight text-slate-400">
-                                                {{ $user->email }}
+                                                {{ $client->email }}
+                                            </span>
+                                        </td>
+
+                                        <td
+                                            class="p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+                                            <span class="text-m font-semibold leading-tight text-slate-400">
+                                                {{ $client->phone }}
                                             </span>
                                         </td>
                                         <td
                                             class="p-2 align-middle text-center bg-transparent border-b whitespace-nowrap shadow-transparent">
-                                            <a href="{{ route('users.edit' , $user->id) }}"
+                                            <a href="{{ route('clients.edit', $client->id) }}"
                                                 class="text-m font-semibold leading-tight text-slate-400">
                                                 Edit </a>
-                                                <form action="{{ route('users.destroy' ,$user->id) }}" method="post">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <button class="text-m font-semibold leading-tight text-slate-400">Delete</button>
-                                                </form>
+                                            <form action="{{ route('clients.destroy', $client->id) }}" method="post">
+                                                @csrf
+                                                @method('delete')
+                                                <button
+                                                    class="text-m font-semibold leading-tight text-slate-400">Delete</button>
+                                            </form>
                                         </td>
                                     </tr>
                                 @endforeach
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,7 @@ Route::group(['middleware' => 'auth'], function () {
     })->name('home');
 
     Route::resource('users', UserController::class);
+    Route::resource('clients', ClientController::class);
 });
 
 Route::group(['middleware' => 'guest'], function () {
